6e48d8a7af956b75650a426f30bf613832df58a7
Anderson Rostirolla <anderson.rostirolla@linx3.com>
2021-04-06 18:55:12 -0300

    fix: gridTemplateColumnValue dynamic based source

--- src/components/WrapperLogin.tsx
+++ src/components/WrapperLogin.tsx
@@ -2,17 +2,23 @@ import React, { useEffect, useState } from 'react';
 import Text from './Text';
 import SignUp from './SignUp';
 import Form from './Form';
+import { isMobile } from '../utils/browser';
 
 interface Props {
   className?: string;
   id?: string;
 }
 
+interface signMode {
+  title: string;
+  className: string;
+}
+
 const WrapperLogin: React.FC<Props> = ({
   className,
   id
 }) => {
-  const signModes = [{
+  const signModes: Array<signMode> = [{
     title: 'facebook',
     className: 'button-login-direct facebook'
   },
@@ -24,9 +30,13 @@ const WrapperLogin: React.FC<Props> = ({
   const [heightGrid, setHeightGrid] = useState('420px');
 
   useEffect(() => {
-    const html = document.querySelector('html');
-    const height = html?.clientWidth || 420;
-    setHeightGrid(`${height}px`);
+    const html: HTMLHtmlElement | null = document.querySelector('html');
+    const height: number = html?.clientWidth || 420;
+    let gridTemplateColumnValue: string = 'none';
+    if (isMobile()) {
+      gridTemplateColumnValue = `${height}px`;
+    }
+    setHeightGrid(gridTemplateColumnValue);
   }, [heightGrid]);
 
   return (
---
+++ src/utils/browser.tsx
@@ -0,0 +1,17 @@
+const touchUserAgents: Array<RegExp> = [
+  /Andoid/i,
+  /webOS/i,
+  /iPhone/i,
+  /iPad/i,
+  /iPod/i,
+  /BlackBerry/i,
+  /Windows Phone/i
+];
+
+export const isTouchDevice = (): boolean => {
+  return touchUserAgents.some(userAgent => window.navigator.userAgent.match(userAgent));
+};
+
+export const isMobile = (): boolean => {
+  return isTouchDevice() && window.screen.width < 1024;
+};
